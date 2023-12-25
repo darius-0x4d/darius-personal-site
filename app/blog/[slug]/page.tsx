2aacf6806f067c57f963c217e2a16a4018cf43da
Darius McFarland <indycolts1893@gmail.com>
2023-12-24 19:15:03 -0500

    #6 Removed contentlayer import

--- app/blog/[slug]/page.tsx
+++ app/blog/[slug]/page.tsx
@@ -1,6 +1,5 @@
 import { notFound } from "next/navigation";
 import { Mdx } from "components/mdx";
-import { allBlogs } from "contentlayer/generated";
 import { getTweets } from "lib/twitter";
 import Balancer from "react-wrap-balancer";
 import ViewCounter from "../view-counter";
